cade758f053b37e5cbdec5ab6d060f19328f76e6
aldy ngere <aldyngere20@gmail.com>
2023-05-25 07:17:51 -0700

    menyelesaikan project change color

---
+++ change_color/script.js
@@ -0,0 +1,31 @@
+// Buat array colors
+const colors = [
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "black",
+  "orange",
+  "purple",
+  "grey",
+  "pink",
+  "orangared",
+  "lightblue",
+  "violet",
+];
+
+// panggil setiap element yang dibutuhkan dari index.html
+let color = document.getElementById("color");
+
+// Buat suatu function sebagai pengkondisian ketika btn color di click
+document.getElementById("btn-color").addEventListener("click", function () {
+  // panggil function getRandomColors dan masukan ke dalam variable baru
+  let randomColors = getRandomColors();
+  document.body.style.backgroundColor = colors[randomColors];
+  color.textContent = colors[randomColors];
+});
+
+// Buat function get random data
+function getRandomColors() {
+  return Math.floor(Math.random() * colors.length);
+}
