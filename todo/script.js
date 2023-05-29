eb1d00c59fbe88b66a3760e116d0ec1d3179da63
aldy ngere <aldyngere20@gmail.com>
2023-05-29 10:45:02 -0700

    menyelesaikan project todo

--- todo/script.js
+++ todo/script.js
@@ -1,57 +1,54 @@
-// Buat array untuk menampung data todo
+// Make todo database
 const dbTodo = [];
-const dbdueTime = [];
+const dbDueTime = [];
 
-// function ambil dan push data jangan lupa gunakan prevetDefault()
-document.forms["todo-form"].onsubmit = (event) => {
-  // ambil lalu push data input ke array
+// Get value and push to database
+document.forms["todo-form"].onsubmit = (e) => {
   let todo = document.forms["todo-form"]["todo"].value;
-  dbTodo.push(todo);
   let dueTime = document.forms["todo-form"]["dueTime"].value;
-  dbdueTime.push(dueTime);
 
-  event.preventDefault();
+  dbTodo.push(todo);
+  dbDueTime.push(dueTime);
+
+  e.preventDefault();
   document.forms["todo-form"].reset();
-  // Lanjutkan proses ke function berikutnya
   dataProcess();
 };
 
-// Function data process
+// Make dataprocess function
 function dataProcess() {
   clearData();
   for (let i = 0; i < dbTodo.length; i++) {
     let todo = dbTodo[i];
-    let dueTime = dbdueTime[i];
+    let dueTime = dbDueTime[i];
 
-    // Lanutkan ke function displayData dengan menambahkan i, todo, dan dueTime
     displayData(i, todo, dueTime);
   }
 }
 
-// Function clear data
+// Clear data first
 function clearData() {
-  const tbody = document.getElementById("todo-tbody");
-  // Buat pengkondisian
+  let tbody = document.querySelector(".todo-tbody");
+  // Make condition
   while (tbody.firstChild) {
     tbody.removeChild(tbody.firstChild);
   }
 }
 
-// Function display data
-function displayData(index, todo, dueTime) {
-  // Table row element
+// Function to display data
+function displayData(i, todo, dueTime) {
   let tr = document.createElement("tr");
 
-  // Table data todo
-  const tdTodo = document.createElement("td");
+  // todo table data
+  let tdTodo = document.createElement("td");
   tdTodo.textContent = todo;
   tr.appendChild(tdTodo);
 
-  // Table data dueTime
-  const tdDueTime = document.createElement("td");
+  // todo table data
+  let tdDueTime = document.createElement("td");
   tdDueTime.textContent = dueTime;
   tr.appendChild(tdDueTime);
 
-  const tbody = document.getElementById("todo-tbody");
+  let tbody = document.querySelector(".todo-tbody");
   tbody.appendChild(tr);
 }
